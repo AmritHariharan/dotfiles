fa05aab9224bea3455b08c62e000e0ce054d1ed6
Amrit Hariharan <amrit@hey.com>
2024-12-27 19:29:55 +0800

    add alacritty, starship, and some helix modifications

--- init.sh
+++ init.sh
@@ -4,3 +4,5 @@ ln -s $(pwd)/zsh/.zshrc ~/.zshrc
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
 ln -s $(pwd)/ssh/config ~/.ssh/config
 ln -s $(pwd)/hx/config.toml ~/.config/helix/config.toml
+ln -s $(pwd)/starship/starship.toml ~/.config/starship.toml
+ln -s $(pwd)/alacritty ~/.config/alacritty
