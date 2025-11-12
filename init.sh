541ae20b3f66d90f1558153500fd3114a574f1f3
Amrit Hariharan <amrit@hey.com>
2025-11-12 00:24:01 -0800

    Move to ghostty, update symlinks

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 # Run this in the dotfiles folder
 ln -sf $(pwd)/zsh/.zshrc ~/.zshrc
 ln -sf $(pwd)/ssh/config ~/.ssh/config
-ln -snf $(pwd)/hx ~/.config/helix
+ln -snf $(pwd)/helix ~/.config
 ln -sf $(pwd)/starship/starship.toml ~/.config/starship.toml
-ln -snf $(pwd)/alacritty ~/.config/alacritty
+ln -snf $(pwd)/ghostty ~/.config
